6cf729af2bd7a31c38ab3cd886749c1542953802
YangGuang <guang334419520@126.com>
2019-02-20 15:33:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## 高效的跳表
+具体的使用方法请看test_skiplist.cc，使用非常简单，根据leveldb的skiplist改进而来
---
+++ test_skiplist.cc
@@ -0,0 +1,36 @@
+﻿
+#include "skiplist.h"
+
+#include <cassert>
+#include <iostream>
+
+int main(void) {
+    base::SkipList<int, int> list;
+    list.Insert(10, 20);
+    list.Insert(20, 30);
+    list.Insert(30, 40);
+    list.Insert(40, 50);
+
+    int result;
+
+    assert(list.LookUp(10, &result));
+    assert(result == 20);
+
+    // 遍历
+    base::SkipList<int, int>::Iterator iter(&list);
+    assert(!iter.Vaild());
+    iter.SeekToFirst();
+    while (iter.Vaild()) {
+        std::cout << "key = " << iter.key() << "  ,  value = " << iter.value() << std::endl;
+        iter.Next();
+    }
+
+    iter.SeekToLast();
+    assert(iter.key() == 40);
+    assert(iter.value() == 50);
+
+    iter.Seek(30);
+    iter.value() = 30;
+    assert(iter.value() == 30);
+    return 0;
+}
